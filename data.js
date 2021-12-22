05c1a46c72956ba31cb12b00d3e3d6936a220d35
criboulet <celian.riboulet@etu.u-bordeaux.fr>
2021-12-22 14:19:58 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,13 @@
+{
+    stops[],
+}
+
+const a = {
+    stops: {
+        12621: "Gradoche",
+        126908: "Bordoche"
+    },
+    lineColor: "#ffffff",
+    lineId: 10,
+    waitInterval: 10
+}
---
+++ src/Controller/SeriesController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Episode;
+use App\Entity\Season;
+use App\Entity\Series;
+use App\Form\SeriesType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/series')]
+class SeriesController extends AbstractController
+{
+    #[Route('/', name: 'series_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $series = $entityManager
+            ->getRepository(Series::class)
+            ->findBy([], null, 20);
+
+        return $this->render('series/index.html.twig', [
+            'series' => $series,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'series_show', methods: ['GET'])]
+    public function show(Series $series, EntityManagerInterface $entityManager): Response
+    {
+        $seasons = $entityManager
+            ->getRepository(Season::class)
+            ->findBy(
+                ['series' => $series->getId()],
+                ['number' => 'ASC']
+            );
+        $seasons_with_epcount = [];
+        foreach($seasons as $s) {
+            $ep_count = count($entityManager
+                ->getRepository(Episode::class)
+                ->findBy(
+                    ['season' => $s->getId()],
+                    ['number' => 'ASC']
+                ));
+            array_push($seasons_with_epcount, [$s, $ep_count]);
+        }
+
+        return $this->render('series/show.html.twig', [
+            'series' => $series,
+            'seasons' => $seasons_with_epcount,
+        ]);
+    }
+
+    #[Route('/poster/{id}', name: 'series_poster', methods: ['GET'])]
+    public function poster(Series $series): Response
+    {
+        $resp = new Response(stream_get_contents($series->getPoster()));
+        $resp->headers->set('Content-Type', 'image/png');
+        return $resp;
+    }
+}
