577fa67dee5fc7927a30536f000dbc231a9be363
celian <celian.riboulet@gmail.com>
2022-01-02 22:38:09 +0100

    Command wip

---
+++ src/Command/GenerateRatings.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateRatings extends Command
+{
+    protected static $defaultName = 'app:generate-ratings';
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln([
+            'Generating ratings',
+            '============',
+        ]);
+
+        return Command::SUCCESS;
+    }
+}
